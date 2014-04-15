1a2cbff6d3193117cf078b644b3ee17e06625a46
Christian Askeland <christian.askeland@sintef.no>
2014-04-15 13:12:17 +0200

    rewrote tutorial to test connection to a service in a plugin

---
+++ Apps/UseCTKPlugin/target_libraries.cmake
@@ -0,0 +1,11 @@
+#
+# See CMake/ctkFunctionGetTargetLibraries.cmake
+# 
+# This file should list the libraries required to build the current CTK application.
+# 
+
+set(target_libraries
+  CTKPluginFramework
+  org_commontk_plugingenerator_core
+  org_mydomain_testplugin
+  )
